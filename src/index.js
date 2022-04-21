42d54ad15dce4d14ef1f68f2dd2e59eda28d4c17
alovega <alovegakevin@gmail.com>
2022-04-21 16:42:20 +0300

    implement page token view

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 import './index.css';
+
+class Book extends React.Component{
+    render(){
+        return(
+            <div className="page">  
+                <div className="row">
+                    With CSS, you have full control over the margins. There are properties for 
+                    setting the margin for each side of an element (top, right, bottom, and left).
+                </div>
+                <div className="row">
+                    With CSS, you have full control over the margins. There are properties for 
+                    setting the margin for each side of an element (top, right, bottom, and left).
+                </div> 
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Book />,
+    document.getElementById('root')
+);
