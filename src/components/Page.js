cb104a7a3e70a8a737324b45676254f36087eede
alovega <alovegakevin@gmail.com>
2022-04-28 09:27:17 +0300

    remove unused files

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,6 +4,7 @@ import PageToken from "./PageToken";
 
 const Page = (props) => {
       const { book }  = props.data;
+      // set state to use in pagination
       const [stateData, setStateData] = useState({
         ...book,
         data: book.pages.slice(0, 2),
@@ -13,7 +14,7 @@ const Page = (props) => {
         stop: 2
       });
       
-  
+      // pagination logic
       const changePage = (direction) => {
         if (direction === 'back') {
           setStateData({ ...stateData,
@@ -34,14 +35,16 @@ const Page = (props) => {
       }
       return(
         <div>
-            <h2 className="header"> title</h2>
+            <h2 className="header"> {stateData.title}</h2>
             <PageToken book={stateData} />
-            {stateData.currentPage > 1 ?
-              <button onClick={() => changePage('back')}>Back</button>
-            : null}
-            {stateData.pages.length -1 > stateData.currentPage * 2 ?
-              <button onClick={() => changePage('next')}>Next</button>
-            : null}
+            <div className="page">
+              {stateData.currentPage > 1 ?
+                <button className="back button" onClick={() => changePage('back')}>Back</button>
+              : null}
+              {stateData.pages.length -1 > stateData.currentPage * 2 ?
+                <button className="next button" onClick={() => changePage('next')}>Next</button>
+              : null}
+            </div>
         </div>
       )
     
